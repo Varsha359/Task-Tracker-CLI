1c992e2a58e79260ca1fb3ca3fdbdefb151f3a6c
Varsha Gajula <gajulavarsha359@gmail.com>
2025-06-22 12:56:39 +0530

    Added model for the task and the json file where we store the task details

--- Task.java
+++ Task.java
@@ -1,3 +1,11 @@
+import java.util.Date;
+
 public class Task {
     // model class for Task
+    private int id;
+    private String description;
+    private String status;
+    private Date createdAt;
+    private Date updatedAt;
+
 }
